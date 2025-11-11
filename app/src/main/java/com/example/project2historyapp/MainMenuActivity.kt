94d981ad9c81170e92834dda69874bf838a23bab
Ciaran Krabak <krck70153@gmail.com>
2025-11-10 21:44:51 -0500

    changed google maps theme

--- app/src/main/java/com/example/project2historyapp/MainMenuActivity.kt
+++ app/src/main/java/com/example/project2historyapp/MainMenuActivity.kt
@@ -9,14 +9,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.project2historyapp.ui.theme.Project2HistoryAppTheme
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -40,9 +44,15 @@ fun MyMap() {
         position = CameraPosition.fromLatLngZoom(staffordVA, 10f)
     }
 
+    val context = LocalContext.current
+
+    // Map Style JSON and code from Google Maps Documentation
+    val mapStyle = remember { MapStyleOptions.loadRawResourceStyle(context, R.raw.map_style) }
+
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState
+        cameraPositionState = cameraPositionState,
+        properties = MapProperties(mapStyleOptions = mapStyle)  // From Google Maps Documentation
     ) {
         Marker(
             state = MarkerState(position =staffordVA),
